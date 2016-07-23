9c331e708d9ad8d545a7b70b5fc5636de1552f81
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2016-07-23 15:40:29 -0300

    user validators

--- module/SocialMediaRestAPI/tests/DAO/UserDAOServiceTest.php
+++ module/SocialMediaRestAPI/tests/DAO/UserDAOServiceTest.php
@@ -7,6 +7,7 @@ use Zend\StdLib\ArrayUtils;
 use SocialMediaRestAPI\DAO\UserDAOInterface;
 use SocialMediaRestAPI\Service\UserDAOService;
 use SocialMediaRestAPI\Model\Entity\User;
+use Core\Model\DAO\Exception\DAOException;
 
 class UserDAOServiceTest extends TestCase 
 {
@@ -21,7 +22,7 @@ class UserDAOServiceTest extends TestCase
         parent::setUp();
     }
 
-    public function testHasBeenRegisteredForDI() {
+    public function testHasBeenRegisteredForDi() {
         $userDAOService = $this->getServiceManager()->get('SocialMediaRestAPI\Service\UserDAOService');
         $this->assertNotNull($userDAOService);
         $this->assertEquals(get_class($userDAOService), UserDAOService::class);
@@ -31,6 +32,7 @@ class UserDAOServiceTest extends TestCase
      * @convers User::getInputFilter
      * @convers User::__set
      * @convers User::__get
+     * @convers User::valid
      */
     public function testUserInputFilterFilters() {
         $user = new User();
@@ -58,8 +60,9 @@ class UserDAOServiceTest extends TestCase
      * @convers User::getData
      * @convers User::getArrayCopy
      * @convers User::toArray
+     * @convers User::valid
      */
-    public function testGetSetDataIntoUser () {
+    public function testGetAndSetDataIntoUser () {
 
         $values = [
             'id' => '01.0',
@@ -92,11 +95,111 @@ class UserDAOServiceTest extends TestCase
     }
 
     /**
-     * @depends testGetSetDataIntoUser
+     * @depends testGetAndSetDataIntoUser
+     * @convers User::valid
      * @convers User::validate
      */
     public function testCanValidateAUser($user) {
         $this->assertTrue($user->validate());
+        return $user;
     }
+
+    /**
+     * @depends testCanValidateAUser
+     * @convers User::valid
+     * @convers User::validate
+     * @expectedException DAOException
+     */
+     public function testCannotSetAUsernameThatIsNotAEmail ($user) {
+         $user->username = "joaozinho da silva";
+     }
+
+    /**
+     * @depends testCanValidateAUser
+     * @convers User::valid
+     * @convers User::validate
+     * @expectedException DAOException
+     */
+     public function testEmailForUsernameMustBeComplete ($user) {
+         $user->username = "joaozinho@";
+     }
+
+    /**
+     * @depends testCanValidateAUser
+     * @convers User::valid
+     * @convers User::validate
+     * @expectedException DAOException
+     */
+     public function testEmailForUsernameMustBeOnly100AtLength ($user) {
+         $user->username = "joaozinhotemumenderecodeemailqueehgrandedemaisenaofazsentidoaquiaindaehcurtoprecisodenovasideiasdetextoipsum@localhost.com";
+     }
+
+    /**
+     * @depends testCanValidateAUser
+     * @convers User::valid
+     * @convers User::validate
+     * @expectedException DAOException
+     */
+     public function testUsernameMustBeInformmed ($user) {
+         $user->username = "   ";
+     }
+
+    /**
+     * @depends testCanValidateAUser
+     * @convers User::valid
+     * @convers User::validate
+     * @expectedException DAOException
+     */
+     public function testUsernameMustBeInformmedAndNotNull ($user) {
+         $user->username = null;
+     }
+
+    /**
+     * @depends testCanValidateAUser
+     * @convers User::valid
+     * @convers User::validate
+     * @expectedException DAOException
+     */
+     public function testPasswordMustBeInformmed ($user) {
+         $user->password = "   ";
+     }
+
+    /**
+     * @depends testCanValidateAUser
+     * @convers User::validate
+     * @expectedException DAOException
+     */
+     public function testPasswordMustBeInformmedAndNotNull ($user) {
+         $user->password = null;
+     }
+
+    /**
+     * @depends testCanValidateAUser
+     * @convers User::valid
+     * @convers User::validate
+     * @expectedException DAOException
+     */
+     public function testNameMustBeInformmed ($user) {
+         $user->name = "   ";
+     }
+
+    /**
+     * @depends testCanValidateAUser
+     * @convers User::validate
+     * @expectedException DAOException
+     */
+     public function testNameMustBeInformmedAndNotNull ($user) {
+         $user->name = null;
+     }
+
+    /**
+     * @depends testCanValidateAUser
+     * @convers User::valid
+     * @convers User::validate
+     * @expectedException DAOException
+     */
+     public function testNameMustBeOnly150AtLength ($user) {
+         $user->username = "Joãozinho tem um nome bem comprido porque alguem tentou trocar o tabeliao por um programa de reconhecimento de voz e não desligaram na hora certa, faltou um pouco";
+     }
 
 }

7739a982a2e2d2342722c67907ed7bbd7f1d88a3
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2016-07-26 23:30:14 -0300

    post dao service tests

--- module/SocialMediaRestAPI/tests/Service/PostDAOServiceTest.php
+++ module/SocialMediaRestAPI/tests/Service/PostDAOServiceTest.php
@@ -2,7 +2,6 @@
 
 namespace SocialMediaRestAPITest\Service;
 
-include_once __DIR__ . '/../Traits/UserTestTrait.php';
 include_once __DIR__ . '/../Traits/PostTestTrait.php';
 
 use Core\Test\TestCase;
@@ -11,15 +10,14 @@ use SocialMediaRestAPI\Service\PostDAOService;
 use SocialMediaRestAPI\Model\Entity\User;
 use SocialMediaRestAPI\Model\Entity\Post;
 use Core\Model\DAO\Exception\DAOException;
-use SocialMediaRestAPITest\Traits\UserTestTrait;
 use SocialMediaRestAPITest\Traits\PostTestTrait;
+use Zend\Paginator\Adapter\AdapterInterface;
 
 /**
  * @author Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
  */
 class PostDAOServiceTest extends TestCase
 {
-    use UserTestTrait;
     use PostTestTrait;
 
     public function setUp() {
@@ -34,4 +32,322 @@ class PostDAOServiceTest extends TestCase
         $this->assertEquals(get_class($postDAOService), PostDAOService::class);
         return $postDAOService;
     }
+
+    /**
+     * @covers PostDAOService::save
+     * @covers PostDAOService::findById
+     * @depends testHasBeenRegisteredForDi
+     */
+    public function testCanCreateAndRetrieve() {
+        $postDAOService = $this->getPostDAOService();
+
+        $user = $this->createGenericUsers(1)[0];
+        $post = $this->newPost('something to say', $user, "2016-07-02 09:30:00");
+
+        $post = $postDAOService->save($post);
+
+        $this->assertEquals($post->id, 1);
+        $this->assertNotNull($post->user);
+        $this->assertEquals($post->user->name, $user->name);
+        $this->assertNotNull($post->datePublish);
+        $this->assertEquals($post->datePublish->format("Y-m-d H:i:s"), '2016-07-02 09:30:00');
+        $this->assertEquals($post->text, 'something to say');
+
+        $post = $postDAOService->findById(1);
+
+        $this->assertEquals($post->id, 1);
+        $this->assertNotNull($post->user);
+        $this->assertEquals($post->user->name, $user->name);
+        $this->assertNotNull($post->datePublish);
+        $this->assertEquals($post->datePublish->format("Y-m-d H:i:s"), '2016-07-02 09:30:00');
+        $this->assertEquals($post->text, 'something to say');
+
+        $post->text = "i changed my mind";
+        $postDAOService->save($post);
+
+        $post = $postDAOService->findById(1);
+
+        $this->assertEquals($post->text, "i changed my mind");
+    }
+
+    /**
+     * @covers PostDAOService::save
+     * @depends testCanCreateAndRetrieve
+     * @expectedExceptionMessage Publish date cannot be changed !
+     */
+    public function testCannotChangeDatePublish() {
+        $post = $this->newPost('something funny', null, "2016-07-07 12:00:01");
+        $postDAOService = $this->getPostDAOService();
+        $post = $postDAOService->save($post);
+
+        $post->datePublish = "2016-07-01 12:00:02";
+
+        $post = $postDAOService->save($post);
+    }
+
+    /**
+     * @covers PostDAOService::save
+     * @depends testCanCreateAndRetrieve
+     * @expectedExceptionMessage User cannot be changed !
+     */
+    public function testCannotChangeUser() {
+        $users = $this->createGenericUsers(2);
+        $post = $this->newPost('something funny', $users[0]);
+        $postDAOService = $this->getPostDAOService();
+        $post = $postDAOService->save($post);
+
+        $post->user = $users[1];
+
+        $post = $postDAOService->save($post);
+    }
+
+    /**
+     * @covers PostDAOService::save
+     * @depends testCanCreateAndRetrieve
+     */
+    public function testCanRemoveAPost() {
+        $post = $this->createGenericPosts(1)[0];
+        $this->assertEquals(1, $post-id);
+
+        $postDAOService = $this->getPostDAOService();
+        $postDAOService->remove($post);
+
+        $post2 = $postDAOService->findById(1);
+        $this->assertNull($post2);
+    }
+
+    /**
+     * @covers PostDAOService::fetchUserPosts
+     * @depends testCanCreateAndRetrieve
+     */
+    public function testCanGetAUsersPost() {
+        $userDAOService = $this->getUserDAOService();
+        $postDAOService = $this->getPostDAOService();
+
+        $user = $this->createGenericUsers(1)[0];
+
+        $posts = [];
+        $posts[] = $this->newPost("a post", $user, "2016-07-01 12:00:00");
+        $posts[] = $this->newPost("a post", $user, "2016-07-01 11:00:00");
+        $posts[] = $this->newPost("a post", $user, "2016-07-03 13:00:00");
+        $posts[] = $this->newPost("a post", $user, "2016-07-01 18:00:00");
+        $posts[] = $this->newPost("a post", $user, "2016-07-02 12:00:00");
+
+        foreach($posts as $post)
+            $postDAOService->save($post);
+        
+        $uPosts = $postDAOService->fetchUserPosts($user);
+        $this->assertCount(5, $uPosts);
+
+        $this->assertEquals("2016-07-03 13:00:00", $uPosts[0]->datePublish);
+        $this->assertEquals("2016-07-02 12:00:00", $uPosts[1]->datePublish);
+        $this->assertEquals("2016-07-01 18:00:00", $uPosts[2]->datePublish);
+        $this->assertEquals("2016-07-01 12:00:00", $uPosts[3]->datePublish);
+        $this->assertEquals("2016-07-01 11:00:00", $uPosts[4]->datePublish);
+
+        $uPosts = $postDAOService->fetchUserPosts($user, 3, 1);
+        $this->assertCount(3, $uPosts);
+
+        // first
+        $this->assertEquals("2016-07-02 12:00:00", $uPosts[0]->datePublish);
+        // last
+        $this->assertEquals("2016-07-01 12:00:00", $uPosts[2]->datePublish);
+    }
+
+    /**
+     * @covers PostDAOService::fetchUserPosts
+     * @depends testCanGetAUsersPost
+     * @expectedExceptionMessage Must inform a user to list posts !
+     */
+    public function testCanotGetPostsFromNobody() {
+        $postDAOService = $this->getPostDAOService();
+        $postDAOService->fetchUserPosts(null);
+    }
+
+    /**
+     * @covers PostDAOService::fetchUserFeed
+     * @depends testCanCreateAndRetrieve
+     */
+    public function testCanGetUsersFeed() {
+        $userDAOService = $this->getUserDAOService();
+        $postDAOService = $this->getPostDAOService();
+
+        $users = $this->createGenericUsers(3);
+        $userDAOService->createFriendship($users[0], $users[1]);
+        $userDAOService->createFriendship($users[2], $users[1]);
+
+        $posts = [];
+
+        // user 1
+        $posts[] = $this->newPost("a post", $users[0], "2016-07-01 12:00:00");
+        $posts[] = $this->newPost("a post", $users[0], "2016-07-01 11:00:00");
+        $posts[] = $this->newPost("a post", $users[0], "2016-07-01 13:00:00");
+        $posts[] = $this->newPost("a post", $users[0], "2016-07-01 18:00:00");
+        $posts[] = $this->newPost("a post", $users[0], "2016-07-02 12:00:00");
+
+        // user 2
+        $posts[] = $this->newPost("a post", $users[1], "2016-07-03 13:00:00");
+        $posts[] = $this->newPost("a post", $users[1], "2016-07-03 12:00:00");
+
+        // user 3
+        $posts[] = $this->newPost("a post", $users[2], "2016-07-01 01:00:00");
+        $posts[] = $this->newPost("a post", $users[2], "2016-07-03 18:00:00");
+
+        foreach($posts as $post)
+            $postDAOService->save($post);
+
+        $feed = $postDAOService->fetchUserFeed($users[0]);
+        $this->assertCount(7, $feed);
+
+        $this->assertEquals("2016-07-03 13:00:00", $feed[0]->datePublish->format("Y-m-d H:i:s"));
+        $this->assertEquals("2016-07-03 12:00:00", $feed[1]->datePublish->format("Y-m-d H:i:s"));
+        $this->assertEquals("2016-07-02 12:00:00", $feed[2]->datePublish->format("Y-m-d H:i:s"));
+        $this->assertEquals("2016-07-01 18:00:00", $feed[3]->datePublish->format("Y-m-d H:i:s"));
+        // last
+        $this->assertEquals("2016-07-01 11:00:00", $feed[6]->datePublish->format("Y-m-d H:i:s"));
+
+        $feed = $postDAOService->fetchUserFeed($users[1]);
+        $this->assertCount(9, $feed);
+
+        $this->assertEquals("2016-07-03 18:00:00", $feed[0]->datePublish->format("Y-m-d H:i:s"));
+        $this->assertEquals("2016-07-03 13:00:00", $feed[1]->datePublish->format("Y-m-d H:i:s"));
+        $this->assertEquals("2016-07-03 12:00:00", $feed[2]->datePublish->format("Y-m-d H:i:s"));
+        $this->assertEquals("2016-07-02 12:00:00", $feed[3]->datePublish->format("Y-m-d H:i:s"));
+        // last
+        $this->assertEquals("2016-07-01 01:00:00", $feed[8]->datePublish->format("Y-m-d H:i:s"));
+
+        $feed = $postDAOService->fetchUserFeed($users[2]);
+        $this->assertCount(4, $feed);
+
+        $this->assertEquals("2016-07-03 18:00:00", $feed[0]->datePublish->format("Y-m-d H:i:s"));
+        $this->assertEquals("2016-07-03 13:00:00", $feed[1]->datePublish->format("Y-m-d H:i:s"));
+        $this->assertEquals("2016-07-03 12:00:00", $feed[2]->datePublish->format("Y-m-d H:i:s"));
+        $this->assertEquals("2016-07-01 01:00:00", $feed[3]->datePublish->format("Y-m-d H:i:s"));
+
+        $feed = $postDAOService->fetchUserFeed($users[0], 5, 1);
+        $this->assertCount(5, $feed);
+
+        // first
+        $this->assertEquals("2016-07-03 12:00:00", $feed[0]->datePublish->format("Y-m-d H:i:s"));
+        // last
+        $this->assertEquals("2016-07-01 18:00:00", $feed[4]->datePublish->format("Y-m-d H:i:s"));
+    }
+
+    /**
+     * @covers PostDAOService::fetchUserFeed
+     * @depends testCanGetUsersFeed
+     * @expectedExceptionMessage Must inform a user to list posts !
+     */
+    public function testCanotGetFeedFromNobody() {
+        $postDAOService = $this->getPostDAOService();
+        $postDAOService->fetchUserFeed(null);
+    }
+
+    /**
+     * @covers PostDAOService::getUserFeedAdapterPaginator
+     * @depends testCanCreateAndRetrieve
+     */
+    public function testGetPaginatedFeed() {
+        $userDAOService = $this->getUserDAOService();
+        $postDAOService = $this->getPostDAOService();
+
+        $users = $this->createGenericUsers(3);
+        $userDAOService->createFriendship($users[0], $users[1]);
+        $userDAOService->createFriendship($users[2], $users[1]);
+
+        $posts = [];
+
+        // user 1
+        $posts[] = $this->newPost("a post", $users[0], "2016-07-01 12:00:00");
+        $posts[] = $this->newPost("a post", $users[0], "2016-07-01 11:00:00");
+        $posts[] = $this->newPost("a post", $users[0], "2016-07-01 13:00:00");
+        $posts[] = $this->newPost("a post", $users[0], "2016-07-01 18:00:00");
+        $posts[] = $this->newPost("a post", $users[0], "2016-07-02 12:00:00");
+
+        // user 2
+        $posts[] = $this->newPost("a post", $users[1], "2016-07-03 13:00:00");
+        $posts[] = $this->newPost("a post", $users[1], "2016-07-03 12:00:00");
+
+        // user 3
+        $posts[] = $this->newPost("a post", $users[2], "2016-07-01 01:00:00");
+        $posts[] = $this->newPost("a post", $users[2], "2016-07-03 18:00:00");
+
+        foreach($posts as $post)
+            $postDAOService->save($post);
+
+        $paginator = $this->getUserFeedAdapterPaginator($users[0]);
+        $this->assertNotNull($paginator);
+        $this->assertTrue($paginator instanceof AdapterInterface, "It is not a AdapterInterface");
+
+        $this->assertEquals(7, $paginator->count());
+        $feed = $paginator->getItems(2, 5);
+        $this->assertCout(4, $feed);
+
+        // first
+        $this->assertEquals("2016-07-02 12:00:00", $feed[0]->datePublish->format("Y-m-d H:i:s"));
+        // last
+        $this->assertEquals("2016-07-01 11:00:00", $feed[3]->datePublish->format("Y-m-d H:i:s"));
+
+        $paginator = $this->getUserFeedAdapterPaginator($users[2]);
+        $this->assertEquals(4, $paginator->count());
+        $feed = $paginator->getItems(2, 5);
+        $this->assertCout(2, $feed);
+
+        // first
+        $this->assertEquals("2016-07-03 12:00:00", $feed[0]->datePublish->format("Y-m-d H:i:s"));
+        // last
+        $this->assertEquals("2016-07-01 01:00:00", $feed[3]->datePublish->format("Y-m-d H:i:s"));
+    }
+
+    /**
+     * @covers PostDAOService::getUserFeedAdapterPaginator
+     * @depends testGetPaginatedFeed
+     * @expectedExceptionMessage Must inform a user to list posts !
+     */
+    public function testCanotGetFeedPaginatorFromNobody() {
+        $postDAOService = $this->getPostDAOService();
+        $postDAOService->getUserFeedAdapterPaginator(null);
+    }
+
+    /**
+     * @covers PostDAOService::save
+     * @depends testCanCreateAndRetrieve
+     */
+    public function testCanGetAUsersPostPaginator() {
+        $userDAOService = $this->getUserDAOService();
+        $postDAOService = $this->getPostDAOService();
+
+        $user = $this->createGenericUsers(1)[0];
+
+        $posts = [];
+        $posts[] = $this->newPost("a post", $user, "2016-07-01 12:00:00");
+        $posts[] = $this->newPost("a post", $user, "2016-07-01 11:00:00");
+        $posts[] = $this->newPost("a post", $user, "2016-07-03 13:00:00");
+        $posts[] = $this->newPost("a post", $user, "2016-07-01 18:00:00");
+        $posts[] = $this->newPost("a post", $user, "2016-07-02 12:00:00");
+
+        foreach($posts as $post)
+            $postDAOService->save($post);
+        
+        $paginator = $postDAOService->getUserPostsAdapterPaginator($user);
+        $this->assertCount(5, $paginator->count());
+        $feed = $paginator->getItems(2, 5);
+        $this->assertCout(3, $feed);
+
+        // first
+        $this->assertEquals("2016-07-01 18:00:00", $feed[0]->datePublish->format("Y-m-d H:i:s"));
+        // last
+        $this->assertEquals("2016-07-01 11:00:00", $feed[2]->datePublish->format("Y-m-d H:i:s"));
+    }
+
+    /**
+     * @covers PostDAOService::getUserPostsAdapterPaginator
+     * @depends testCanGetAUsersPostPaginator
+     * @expectedExceptionMessage Must inform a user to list posts !
+     */
+    public function testCanotGetUserPostsPaginatorFromNobody() {
+        $postDAOService = $this->getPostDAOService();
+        $postDAOService->getUserPostsAdapterPaginator(null);
+    }
+
 }
--- module/SocialMediaRestAPI/tests/Traits/PostTestTrait.php
+++ module/SocialMediaRestAPI/tests/Traits/PostTestTrait.php
@@ -2,8 +2,12 @@
 
 namespace SocialMediaRestAPITest\Traits;
 
+include_once __DIR__ . '/../Traits/UserTestTrait.php';
+
 use SocialMediaRestAPI\Model\Entity\Post;
 use SocialMediaRestAPI\Service\PostDAOService;
+use SocialMediaRestAPITest\Traits\UserTestTrait;
+use DateTime;
 
 /**
  * Some methods to help test post related things
@@ -14,6 +18,8 @@ use SocialMediaRestAPI\Service\PostDAOService;
  */
 trait PostTestTrait {
 
+    use UserTestTrait;
+
     /**
      * Retrieve the <code>PostDAOService</code>
      * @return PostDAOService
@@ -22,4 +28,40 @@ trait PostTestTrait {
         return $this->getServiceManager()->get('SocialMediaRestAPI\Service\PostDAOService');
     }
 
+    /**
+     * Create a new Post based on parameters
+     * @param string $text
+     * @param User $user
+     * @param string|DateTime $datePublish
+     * @return Post
+     */
+    private function newPost($text = "a generic post", User $user = null, $datePublish = null) {
+        
+        $datePublish = $datePublish === null ? new DateTime() : $datePublish;
+        $user = $user !== null ? $user : $this->createGenericUsers(1)[0];
+        
+        $post = new Post();
+        $post->user = $user;
+        $post->datePublish = $datePublish;
+        $post->text = $text;
+        
+        return $post;
+    }
+
+    /**
+     * Create generic posts for a user
+     * @param User $user
+     * @param int $qtt number of posts to be created
+     * @return array posts
+     */
+    private function createGenericPosts(User $user, $qtt) {
+        $postDAOService = $this->getPostDAOService();
+        $posts = [];
+        for($i = 1; $i <= $qtt; $i++) {
+            $posts[] = $postDAOService->save(
+                $this->newPost("the post number $i from $user->name", $user)
+            );
+        }
+        return $posts;
+    }
 }

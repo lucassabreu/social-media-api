b5f47446f96eefaf9482508bf117fa83b32936e7
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2017-01-15 10:02:01 -0200

    Phpunit correction (#1)
    * build
    
    * use sqlite
    
    * dockerize
    
    * docker image

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM php:7
 
 RUN apt-get update && \
-    apt-get install -y zip git
-RUN docker-php-ext-install pdo_mysql \
-    && docker-php-ext-enable pdo_mysql
+    apt-get install -y wget zip git libfreetype6-dev \
+        libmcrypt-dev libssl-dev libicu-dev libsqlite3-dev
+
+# Install PHP extensions
+RUN docker-php-ext-install -j$(nproc) pdo pdo_sqlite pdo_mysql intl
--- config/test/autoload/test.local.php
+++ config/test/autoload/test.local.php
@@ -5,24 +5,11 @@ return [
     'doctrine' => [
         'connection' => [
             'orm_default' => [
-                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'driverClass' => \Doctrine\DBAL\Driver\PDOSqlite\Driver::class,
                 'params' => [
-                    'host'     => 'localhost',
-                    'port'     => '3306',
-                    'user'     => 'root',
-                    'password' => 'root',
-                    'dbname'   => 'social-media-test',
+                    'path' => '/tmp/test.sqlite'
                 ]
             ]
         ]
-    ],
-    'db' => [
-        'driver' => 'PDO',
-        'dsn' => 'mysql:host=localhost;dbname=social-media-test',
-        'username' => 'root',
-        'password' => 'root',
-        'driver_options' => array(
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-        ),
     ],
 ];
